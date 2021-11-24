9435dafa84e02ed8be6a0ebd91a02666436e7684
dralexsand <dr.alex.sand.111@gmail.com>
2021-11-24 13:22:45 +0300

    clean code

--- config/app.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'deploy' => getenv('DEPLOY', 'local'),
-    'debug' => getenv('DEBUG', true),
-    'app_name' => getenv('APP_NAME', 'Mirai Test'),
-    'logs' => getenv('APP_ROOT').env('LOGS'),
-];
--- config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'db_host' => getenv('DB_HOST', 'localhost'),
-    'db_name' => getenv('DB_NAME', 'ft_extra'),
-    'db_user' => getenv('DB_USER', 'root'),
-    'db_pass' => getenv('DB_PASS', 'root'),
-];
